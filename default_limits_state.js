db659620ea8bae7cb9e4496693457d951f1fd132
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-05-24 13:24:41 -0400

    quite good

--- default_limits_state.js
+++
@@ -1,2 +0,0 @@
-position[0] = [0, 500]
-position[1] = [0, 500]
--- default_render.js
+++ default_render.js
@@ -1,9 +1,14 @@
 function render(ctx, state, action) {
-    console.log('Rendering, state:', state, 'action:', action);
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.fillStyle = 'blue';
+
     ctx.beginPath();
-    ctx.arc(state["position[0]"], state["position[1]"], 20, 0, 2 * Math.PI);
-    ctx.stroke();
+    ctx.arc(state.position[0], state.position[1], action[0], 0, 2 * Math.PI);
+    ctx.fillStyle = 'blue';
     ctx.fill();
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(state.position[0], state.position[1]);
+    ctx.lineTo(state.position[0] + Math.cos(state.orientation.test) * 50, state.position[1] + Math.sin(state.orientation.test) * 50);
+    ctx.stroke();
 }
